c9aadb4ca9333c0fe85c4e492819a1e6b97ca469
Lalima Singh <144648165+Lalima-tmr@users.noreply.github.com>
2024-03-21 14:29:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
-document.onscroll = () => {
-    if (window.scrollY > 0) {
-      document.querySelector('.header').classList.add('active');
-    } else {
-      document.querySelector('.header').classList.remove('active');
-    }
-  };
-  
-  document.onload = () => {
-    if (window.scrollY > 0) {
-      document.querySelector('.header').classList.add('active');
-    } else {
-      document.querySelector('.header').classList.remove('active');
-    }
-  };
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-  const navbar = document.querySelector('.header .navbar');
-const menuButton = document.querySelector('.header .menu');
+menuIcon.onclick =() => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
-menuButton.addEventListener('click', () => {
-  navbar.classList.toggle('show');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll=() => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+        };
+    });
+
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY >100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
 });
 
-document.onscroll = () => {
-  navbar.classList.remove('show');
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right' });
 
-  if (window.scrollY > 0) {
-    document.querySelector('.header').classList.add('active');
-  } else {
-    document.querySelector('.header').classList.remove('active');
-  }
-};
+const typed = new Typed ('.multiple-text', {
+    strings: ['Frontend Developer','Frontend Developer','Frontend Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
 
-document.onload = () => {
-  if (window.scrollY > 0) {
-    document.querySelector('.header').classList.add('active');
-  } else {
-    document.querySelector('.header').classList.remove('active');
-  }
-};
+});
